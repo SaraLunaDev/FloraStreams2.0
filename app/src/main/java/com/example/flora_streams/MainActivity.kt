3cb540f1d0ae9609e0ff6e24827bb89847194c8b
SaraLunaDev <blacktsukibiz@gmail.com>
2025-09-13 18:35:27 +0200

    Initial commit

---
+++ app/src/main/java/com/example/flora_streams/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.flora_streams
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.flora_streams.ui.theme.Flora_streamsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
